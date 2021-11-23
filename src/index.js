f229ec736611986c78488222e7f6f92235b6da01
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-23 09:50:45 +0000

    Queries/Fetching example completed

--- src/index.js
+++ src/index.js
@@ -27,10 +27,35 @@ client
   })
   .then((result) => console.log(result));
 
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
 function App() {
   return (
     <div>
       <h2>My first Apollo app</h2>
+      <ExchangeRates />
     </div>
   );
 }
