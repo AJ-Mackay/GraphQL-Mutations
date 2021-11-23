19ca3ffe466c3dee76d5bef843f75a9d700af69d
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-23 10:05:52 +0000

    Caching query results

--- src/index.js
+++ src/index.js
@@ -11,51 +11,68 @@ import {
 import './index.css';
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'https://71z1g.sse.codesandbox.io',
   cache: new InMemoryCache(),
 });
 
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
-
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
+const GET_DOGS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
     }
   }
 `;
 
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+function Dogs({ onDogSelected }) {
+  const { loading, error, data } = useQuery(GET_DOGS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
 
-  return data.rates.map(({ currency, rate }) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ));
+  return (
+    <select name="dog" onChange={onDogSelected}>
+      {data.dogs.map((dog) => (
+        <option key={dog.id} value={dog.breed}>
+          {dog.breed}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+const GET_DOG_PHOTO = gql`
+  query Dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+function DogPhoto({ breed }) {
+  const { loading, error, data } = useQuery(GET_DOG_PHOTO, {
+    variables: { breed },
+  });
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+
+  return (
+    <img
+      alt="dog"
+      src={data.dog.displayImage}
+      style={{ height: 100, width: 100 }}
+    />
+  );
 }
 
 function App() {
   return (
     <div>
-      <h2>My first Apollo app</h2>
-      <ExchangeRates />
+      <h2>My first Apollo Fetch Query app</h2>
+      <Dogs />
+      <DogPhoto />
     </div>
   );
 }
